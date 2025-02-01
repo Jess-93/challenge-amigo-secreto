1d859169d0c0a3a22d4acbd4742aeae9443fcf23
Jesús Villarreal <bash1@live.com.mx>
2025-01-31 22:55:54 -0800

    Se hicieron correcciones ortográficas en los comentarios

--- app.js
+++ app.js
@@ -8,25 +8,25 @@ function agregarAmigo() {
 
     // Validar caracteres especiales
     if (textoIngresado.match(/[!@#$%^&*(),.?":{}|<>]/)) {
-        alert("El nombre no puede contener caracteres especiales");
+        alert("El nombre no puede contener caracteres especiales.");
         return false;
     }
 
     // Validar campo vacío
     if (textoIngresado === "") {
-        alert("Por favor escriba un nombre");
+        alert("Por favor escriba un nombre.");
         return false;
     }
 
     // Validar números en el texto
     if (textoIngresado.match(/\d/)) {
-        alert("El texto no puede contener números");
+        alert("El texto no puede contener números.");
         return false;
     }
 
     // Verificar si el nombre ya fue ingresado
     if (amigos.includes(textoIngresado)) {
-        alert("El nombre ya fue ingresado, por favor elija otro");
+        alert("El nombre ya fue ingresado, por favor elija otro.")
         return false;
     }
 
